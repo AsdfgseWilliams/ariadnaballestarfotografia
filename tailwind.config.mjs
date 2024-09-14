6381214e6b0707bad34d3aa6338b30e175c6d3dd
AsdfgseWilliams <ariadna.ballestar.mateo@gmail.com>
2024-09-14 09:37:07 +0200

    Primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'grisclarito': '#f6f6f4',
+		},
+		},
+		fontFamily: {
+			'claster': ['Claster', 'serif'],
+			'eb': ['EBGaramond', 'sans-serif'],
+			'overpass': ['Overpass', 'sans-serif'],
+		},
+	},
+	plugins: [],
+}
