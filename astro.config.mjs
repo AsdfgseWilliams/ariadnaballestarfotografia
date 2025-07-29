d7bb138ced9b6180e6ac6942fcea96c534ad4294
AsdfgseWilliams <ariadna.ballestar.mateo@gmail.com>
2025-07-29 22:04:46 +0200

    traducciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,11 +9,12 @@ export default defineConfig({
   site: 'https://AsdfgseWilliams.github.io',
   base: '/',
   output: 'server',
-   i18n: {
+  adapter: vercel(),
+  i18n: {
         defaultLocale: 'es',
         locales: ['es', 'en'],
         routing: {
             prefixDefaultLocale: false,
         }
-    },
+  },
 });
