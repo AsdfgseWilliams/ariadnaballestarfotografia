9e8c884e10c47d5b3046d64fe459222fce6cc6e8
AsdfgseWilliams <ariadna.ballestar.mateo@gmail.com>
2025-07-30 21:57:11 +0200

    Añadir primeras traducciones más slugs

---
+++ src/i18n/slug-map.ts
@@ -0,0 +1,28 @@
+// src/i18n/slug-map.ts
+
+export const slugMap = {
+  bodas: {
+    es: 'bodas',
+    en: 'weddings',
+  },
+  sesiones: {
+    es: 'sesiones',
+    en: 'photoshoots',
+  },
+  'sobre-mi': {
+    es: 'sobre-mi',
+    en: 'about',
+  },
+  contacto: {
+    es: 'contacto',
+    en: 'contact',
+  },
+  precios: {
+    es: 'precios',
+    en: 'prices',
+  },
+  blog: {
+    es: 'blog',
+    en: 'blog',
+  },
+};
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,12 +10,35 @@ export const showDefaultLang = false; // Cambiar a false para no mostrar /es
 
 export const ui: Record<string, Record<string, Record<string, string>>> = {
         en: {
+
+        button: {
+            '1': 'Contact me!',
+            '2': 'Get to know me better',
+            'ig': 'Instagram',
+        },
         home: {
-            'h1.hero': 'Wedding photographer in Barcelona',
+            'hero.h1': 'Wedding photographer in Barcelona',
             'hero.p1': 'In the warm embrace of Barcelona, a city that throbs with romance and charm, your wedding memories take on an even more magical dimension.',
             'hero.p2': 'In each memory lies a universe of sensations, laughter, tears, learning and achievements. They are fundamental pillars that support the construction of your own history, shape your experiences and forge your vision of the world.',
             'hero.p3': 'With every click, we weave a timeless story, transforming fleeting moments into unforgettable memories at weddings in Barcelona.',
-            'hero.cta': 'Contact me!'
+
+            'about.h2': 'Hi, I’m Ariadna, a wedding photographer based in Barcelona.',
+            'about.p1': 'Wedding photographer in Barcelona, also specialising in couples, portraits and family sessions.',
+            'about.p2': 'I’m glad you’re here to find out how I’m going to tell your story.',
+            'about.p3': 'The day you vowed eternal love to each other? A different photo session as a couple? A session just for you? A beautiful and fun family memory?',
+            'about.p4': 'Whatever it is, <span class="underline">I know you and I listen to your wishes</span> so that the final result is as special as you imagine it to be.',
+            'about.p5': 'And when the day comes, <span class="underline"> I accompany you</span>  so that you can enjoy every moment and detail with confidence, while I silently portray indescribable emotions for life.',
+            'about.quote': 'To succeed, your desire to succeed must be greater than your fear of failure.',
+            'about.p6': 'I love exploring new ideas through photography. I am most inspired by everyday things. For me, inspiration comes in the smallest forms: shadows, movement, light, lines and conversations. My work is a celebration of life, and I want to bring sunshine into your everyday.',
+            'about.p7': 'My approach to photography is <strong>capturing the beauty of the ordinary and transforming it into something exceptional.</strong> Each image tells a unique story and I seek to capture the essence of each moment. I work with couples who are looking to capture their love and connection in a setting that reflects their personality and style.',
+            'about.p8': 'I am passionate about the opportunity to travel and discover new cultures, and I love to incorporate cultural and artistic elements into my photo shoots. My goal is to create images that speak for themselves and make you feel something special every time you see them.',
+
+            'servicio1': 'PORTRAITS',
+            'servicio2': 'WEDDINGS',
+            'servicio3': 'COUPLES',
+
+            'cta.h3' : 'A compilation of my latest works',
+            'cta.p' : 'Photographs are not only proof that you have lived; they are proof that you have loved.',
         },
         nav: {
             'bodas': 'Weddings in Barcelona',
@@ -31,13 +54,35 @@ export const ui: Record<string, Record<string, Record<string, string>>> = {
         
     },
     es: {
+
+        button: {
+            '1': '¡Contacta conmigo!',
+            '2': 'Conóceme más',
+            'ig': 'Instagram',
+        },
         home: {
-            'h1.hero': 'Fotógrafa de bodas en Barcelona',
+            'hero.h1': 'Fotógrafa de bodas en Barcelona',
             'hero.p1': 'En el cálido abrazo de Barcelona, una ciudad que palpita con romance y encanto, tus recuerdos de boda cobran una dimensión aún más mágica.',
             'hero.p2': 'En cada recuerdo yace un universo de sensaciones, risas, lágrimas, aprendizajes y logros. Son pilares fundamentales que sustentan la construcción de tu propia historia, moldean tus experiencias y forjan tu visión del mundo.',
             'hero.p3': 'En cada clic, tejemos una historia eterna, transformando momentos fugaces en recuerdos inolvidables en las bodas de Barcelona.',
-            'hero.cta': '¡Contacta conmigo!'
-            
+
+            'about.h2': 'Hola, Soy Ariadna, fotógrafa de bodas en Barcelona.',
+            'about.p1': 'Fotógrafa de bodas en Barcelona, especializada también en parejas, retratos y sesiones familiares.',
+            'about.p2': 'Me alegra que estés aquí para que descubras cómo voy a contar tu historia.',
+            'about.p3': '¿El día que os juráis amor eterno? ¿Unas fotos diferentes en pareja? ¿Una sesión solo para ti? ¿Un recuerdo bonito y divertido en familia?',
+            'about.p4': 'Sea cual sea, <span class="underline">te conozco y escucho tus deseos</span> para que el resultado final sea tan especial como lo imaginas.',
+            'about.p5': 'Y cuando llega el día, <span class="underline">te acompaño</span> para que disfrutes de cada instante y detalle con confianza, mientras voy retratando, silenciosa, emociones indescriptibles para toda la vida.',
+            'about.quote': 'Para triunfar, tu deseo de éxito debe ser mayor que tu miedo al fracaso.',
+            'about.p6': 'Me encanta explorar nuevas ideas a través de la fotografía. Lo que más me inspira son las cosas cotidianas. Para mí, la inspiración viene en las formas más pequeñas: sombras, movimiento, luz, líneas y conversaciones. Mi trabajo es una celebración de la vida, y quiero llevar el sol a tu día a día.',
+            'about.p7': 'Mi enfoque en la fotografía es <strong>capturar la belleza de lo ordinario y transformarlo en algo excepcional.</strong> Cada imagen cuenta una historia única y busco plasmar la esencia de cada momento. Trabajo con parejas que buscan capturar su amor y su conexión en un entorno que refleje su personalidad y estilo.',
+            'about.p8': 'Me apasiona la oportunidad de viajar y descubrir nuevas culturas, y me encanta incorporar elementos culturales y artísticos en mis sesiones de fotos. Mi objetivo es crear imágenes que hablen por sí solas y que te hagan sentir algo especial cada vez que las veas.',
+
+            'servicio1': 'RETRATOS',
+            'servicio2': 'BODAS',
+            'servicio3': 'PAREJAS',
+
+            'cta.h3' : 'Una recopilación de mis últimos trabajos ',
+            'cta.p' : ' Las fotografías no son sólo la prueba de que has vivido; son la prueba de que has amado. ',
         },
         nav: {
             'bodas': 'Bodas en Barcelona',
