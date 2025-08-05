49d9cafa8e0ebd5cf41f113b97f9f7e17af56d78
AsdfgseWilliams <ariadna.ballestar.mateo@gmail.com>
2025-08-05 20:42:42 +0200

    traducciones

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,13 +9,50 @@ export const showDefaultLang = false; // Cambiar a false para no mostrar /es
 
 
 export const ui: Record<string, Record<string, Record<string, string>>> = {
-        en: {
+    en: {
+
+        nav: {
+            'bodas': 'Weddings in Barcelona',
+            'sesiones': 'Photoshoots',
+            'sobre-mi': 'About Me',
+            'contacto': 'Contact',
+            'precios': 'Prices',
+            'blog': 'Blog',
+            'cta': "LET'S GET STARTED!",
+
+        },
+
+        opiniones: {
+            'title1': '"Great professional and a better person"."',
+            'content1': 
+            "She has been very easy to work with. <br>Very responsive to our needs and very easy to work with.<br>We have recommended her and if we ever need someone again we would count on her without hesitation.<br>A very sweet person who reflects this in her work.",
+            'author1': "- CRISTINA & TONI -",
+
+            'title2': '"She was very approachable and professional, making us feel very comfortable and confident."',
+            'content2': 
+            "Unbeatable.<br> We are delighted with Ariadna, she knew how to capture our style perfectly. She was very approachable and professional, making us feel very comfortable and confident. On the day of the wedding she reassured us and helped us at all times. <br> We chose both photos and video, and the quality has been unbeatable. <br>You delivered all the material very promptly, beautiful and personalised down to the smallest detail. We are very grateful for everything and highly recommend 1000%. We look forward to working with her again.<br> Thank you for being part of our big day!",
+            "author2": "- MARTA & ESMAIL -",
+
+            'title3': '" Feelings in pictures"',
+            'content3':
+            "Ariadna is understanding and attentive.<br>He didn't miss any details of the wedding. And we didn't notice he was there.<br>He knows how to capture the exact moment and guide you so that the images describe you.<br>Clearly see the angle for the photos and bring out the best of the event in every image.<br>You get attached to it and everything.",
+            'author3': "- DIANA & GEMI –",
+        },
 
         button: {
             '1': 'Contact me!',
             '2': 'Get to know me better',
             'ig': 'Instagram',
+            'cta' : 'Book a session',
+
         },
+
+        cta2:{
+            title: 'I CAN’T WAIT TO CAPTURE YOUR WEDDING DAY.',
+            paragraph: 'Immortalising your love story with every click, capturing moments full of magic and emotion.',
+            buttonText: 'Work with me',
+        },
+
         home: {
             'hero.h1': 'Wedding photographer in Barcelona',
             'hero.p1': 'In the warm embrace of Barcelona, a city that throbs with romance and charm, your wedding memories take on an even more magical dimension.',
@@ -39,26 +76,155 @@ export const ui: Record<string, Record<string, Record<string, string>>> = {
 
             'cta.h3' : 'A compilation of my latest works',
             'cta.p' : 'Photographs are not only proof that you have lived; they are proof that you have loved.',
-        },
-        nav: {
-            'bodas': 'Weddings in Barcelona',
-            'sesiones': 'Photoshoots',
-            'sobre-mi': 'About Me',
-            'contacto': 'Contact',
-            'precios': 'Prices',
-            'blog': 'Blog',
-            'cta': "LET'S GET STARTED!",
+            'cta.h3.2' : 'Let me capture your story!',
+            'cta.p.2' : 'Freeze this incredible moment so that you can always remember it with joy and happiness.',
+            'trabajos.h2' : 'MY WORK',
 
+            'cta3.h2' : 'My photography captures authentic connections and powerful messages in emotive images.',
+            'cta3.p' : 'My aim is to create images that reflect the beauty, emotion and love present in every detail, so that they can be treasured by the couple and their loved ones forever.',
+            'cta3.button' : 'See what I have to offer',
         },
 
+        about: {
+
+            'h1' : 'I AM ARIADNA<br> you and your story are my inspiration',
+            'h2' : 'Living in Barcelona, available worldwide.',
+            'h3' :'I accompany you in the most special moments of your life to create unforgettable memories.',
+            'p1' : 'Each couple is unique. That’s why I believe that the magic of photography is to listen and connect with the people I work with, to capture their uniqueness and to tell with naturalness emotions and experiences that will last forever. ',
+            'p2' : '"When your passion is your work, you give the best of yourself and you transmit enthusiasm and professionalism in every detail"',
+            'p3' : 'In my work I have met wonderful stories and people.',
+            'p4' : 'Together we make a good team: they live and enjoy the experience, and I, behind the camera, narrate in images the happiness of unrepeatable moments.',
+            'p5' : 'More than a photographer, I will be a friend who captures your most special moments.',
+            'p6' : 'Tell me your story and let’s create a memory forever!',
+
+            'kpi1' : 'Years of capturing emotions',
+            'kpi2' : 'Happy customers',
+            'kpi3' : 'Photographed weddings',
+            'kpi4' : 'Love for my work',
+
+            'me.h2' : 'CLASSIC AND ROMANTIC WEDDING PHOTOS<br> that will stand the test of time',
+            'me.p1' : 'By your side, from the beginning to the end.',
+            'me.p2' : 'In the tiniest moments and in the smallest details, I will be there to stop time and create memories that will thrill you and make you relive that happiness over and over again.',
+            'me.p3' : 'My commitment is to make you feel comfortable and confident at all times, so that you can be yourself and enjoy every moment to the fullest. Before the big day, we will talk in depth about your wishes and expectations, so that you can trust me and feel the peace of mind of knowing that your story is in good hands.',
+            'me.p4' : 'When you see the photographs, you will be able to relive those moments full of emotion and joy, and feel how time stands still for an instant.',
+
+            'f1' : 'GUILTY PLEASURE',
+            'ff1' : 'Naps',
+            'f2' : 'DREAMED JOURNEY',
+            'ff2' : 'Nordic countries',
+            'f3' : 'PASSIONATE ABOUT',
+            'ff3' : 'The animals',
+            'f4' : 'I CAN’T LIVE WITHOUT',
+            'ff4' : 'My cat',
+            'f5' : 'FAVOURITE FILM',
+            'ff5' : 'The Lord of the Rings',
+            'f6' : 'FAVOURITE MUSIC GROUP',
+            'ff6' : 'Guns N’ Roses',
+            'f7' : 'BEACH OR MOUNTAIN',
+            'ff7' : 'Mountain always',
+            'f8' : 'Curious HOBBY',
+            'ff8' : 'Addicted to Sudoku',
+
+
+            'timelineTitle': 'MY JOURNEY SO FAR',
+            'timelineSubtitle': 'WHERE IT ALL STARTED AND WHERE IT IS NOW',
+
+            'timeline.year1': "2012",
+            'timeline.title1': "I bought my first camera.",
+            'timeline.description1': "I needed it for my photography class at university. That’s how I started practicing and falling in love.",
+            'timeline.imgSrc1': "/img/18.webp",
+            'timeline.alignment1': "left",
+
+            'timeline.year2': "2017",
+            'timeline.title2': "I said yes to photography and started my project.",
+            'timeline.description2': "I finished my Fine Arts studies and decided to start my adventure as a photographer.",
+            'timeline.imgSrc2': "/img/19-1.webp",
+            'timeline.alignment2': "right",
+
+            'timeline.year3': "2018",
+            'timeline.title3': "From Barcelona to Lleida.",
+            'timeline.description3': "I changed city and settled as a photographer in Lleida, specializing in weddings, couples, solo sessions, and families.",
+            'timeline.imgSrc3': "/img/20.webp",
+            'timeline.alignment3': "left",
+
+            'timeline.year4': "2023",
+            'timeline.title4': "Back to my beloved Barcelona.",
+            'timeline.description4': "After a pandemic, I returned to the city where it all started, full of excitement and hope.",
+            'timeline.imgSrc4': "/img/21.webp",
+            'timeline.alignment4': "right",
+
+            'timelineFuture' : '2024 & BEYOND',
+        },
+
+        form: {
+            'title': "CONTACT FORM",
+
+            'name': "Name",
+            'email': "Email",
+            'phone': "Phone",
+            'date': "Event date",
+            'location': "Where will it be?",
+
+            'reservationPlaceholder': "What do you want to book?",
+            'reservationWedding': "Wedding",
+            'reservationCouple': "Couple session",
+            'reservationIndividual': "Individual session",
+            'reservationFamily': "Family session",
+            'reservationOthers': "Others",
+
+            'knownPlaceholder': "Where did you meet me?",
+            'knownInstagram': "Instagram",
+            'knownGoogle': "Google",
+            'knownRecommendation': "Recommendation",
+
+            'details': "Tell me more details",
+
+            'privacyPolicy': "Please, review the privacy policy.",
+
+            'submit': "Submit",
+        }
         
     },
     es: {
+
+         nav: {
+            'bodas': 'Bodas en Barcelona',
+            'sesiones': 'Sesiones',
+            'sobre-mi': 'Sobre mí',
+            'contacto': 'Contacto',
+            'precios': 'Precios',
+            'blog': 'Blog',
+            'cta': '¡EMPECEMOS!',
+        },
+
+        opiniones: {
+            'title1': '"Gran profesional y mejor persona"',
+            'content1': 
+            "Han sido todo facilidades con ella. <br>Muy sensible a nuestras necesidades y muy fácil trabajar con ella. <br>La hemos recomendado y si volviésemos a necesitar a alguien contaríamos con ella sin dudar.<br>Una persona muy dulce que lo refleja en su trabajo.",
+            'author1': "- CRISTINA & TONI -",
+
+            'title2': '"Fue muy cercana y profesional, haciéndonos sentir muy cómodos y con total confianza."',
+            "content2": 
+            "Inmejorable.<br> Estamos encantados con Ariadna, supo captar a la perfección nuestro estilo. Fue muy cercana y profesional, haciéndonos sentir muy cómodos y con total confianza. El día de la boda nos tranquilizó y ayudó en todo momento. <br> Elegimos tanto fotos como vídeo, y la calidad ha sido inmejorable.<br>Nos entregó todo el material muy pronto, precioso y personalizado hasta el más mínimo detalle. Estamos muy agradecidos por todo y la recomendamos 1000%. Esperamos volver a trabajar con ella.<br> ¡Gracias por formar parte de nuestro gran día!",
+            "author2": "- MARTA & ESMAIL -",
+
+            'title3': '"Sentimiento en imágenes"',
+            'content3':
+            "Ariadna es comprensiva y atenta.<br>No perdió detalle de la boda. Y no notamos que estaba allí.<br>Sabe captar el momento exacto y guiarte para que las imágenes describan tu.<br>Ve con claridad el ángulo para las fotos y saca a relucir lo mejor del evento en cada imagen.<br>Le coges cariño y todo.",
+            'author3': "- DIANA & GEMI –",
+        },
 
         button: {
             '1': '¡Contacta conmigo!',
             '2': 'Conóceme más',
             'ig': 'Instagram',
+            'button.cta' : 'Reserva una sesión',
+        },
+
+        cta2:{
+            title: 'NO PUEDO ESPERAR PARA CAPTURAR EL DÍA DE VUESTRA BODA',
+            paragraph: 'Inmortalizando vuestra historia de amor con cada clic, capturando momentos llenos de magia y emoción.',
+            buttonText: 'Trabajemos juntos',
         },
         home: {
             'hero.h1': 'Fotógrafa de bodas en Barcelona',
@@ -83,17 +249,115 @@ export const ui: Record<string, Record<string, Record<string, string>>> = {
 
             'cta.h3' : 'Una recopilación de mis últimos trabajos ',
             'cta.p' : ' Las fotografías no son sólo la prueba de que has vivido; son la prueba de que has amado. ',
+            'cta.h3.2' : '¡Permíteme capturar vuestra historia!',
+            'cta.p.2' : 'Congelar este momento increíble para que puedas recordarlo siempre con alegría y felicidad.',
+            'trabajos.h2' : 'MIS TRABAJOS',
+
+            'cta3.h2' : 'Mi fotografía capta conexiones auténticas y mensajes poderosos en imágenes emotivas.',
+            'cta3.p' : 'Mi objetivo es crear imágenes que reflejen la belleza, la emoción y el amor presentes en cada detalle, para que puedan ser atesoradas por la pareja y sus seres queridos por siempre.',
+            'cta3.button' : 'Mira lo que ofrezco',
         },
-        nav: {
-            'bodas': 'Bodas en Barcelona',
-            'sesiones': 'Sesiones',
-            'sobre-mi': 'Sobre mí',
-            'contacto': 'Contacto',
-            'precios': 'Precios',
-            'blog': 'Blog',
-            'cta': '¡EMPECEMOS!',
+
+        about: {
+
+            'h1' : 'SOY ARIADNA<br> tú y tu historia sois mi inspiración.',
+            'h2' : 'Viviendo en Barcelona, disponible en todo el mundo.',
+            'h3' :'Te acompaño en los momentos más especiales de tu vida para crear recuerdos imborrables.',
+            'p1' : 'Cada pareja es única. Por eso creo que la magia de la fotografía es escuchar y conectar con las personas que trabajo, para captar su singularidad y contar con naturalidad emociones y vivencias que perdurarán para siempre.',
+            'p2' : '“Cuando tu pasión es tu trabajo das lo mejor de ti y transmites en cada detalle ilusión y profesionalidad”.',
+            'p3' : 'En mi trabajo he conocido historias y personas maravillosas.',
+            'p4' : 'Juntas formamos un buen equipo: ellas viven y disfrutan la experiencia, y yo, tras la cámara, relato en imágenes la felicidad de momentos irrepetibles.',
+            'p5' : 'Más que una fotógrafa, seré una amiga que captura vuestros momentos más especiales.',
+            'p6' : '¡Contadme vuestra historia y creemos un recuerdo para siempre!',
+
+            'kpi1' : 'Años capturando emociones',
+            'kpi2' : 'Clientes felices',
+            'kpi3' : 'Bodas fotografiadas',
+            'kpi4' : 'Amor por mi trabajo',
+
+            'me.h2' : 'FOTOS DE BODA CLÁSICAS Y ROMÁNTICAS<br> que resistirán el paso del tiempo',
+            'me.p1' : 'A vuestro lado, desde el principio hasta el final.',
+            'me.p2' : 'En los momentos más sencillos y en los detalles más pequeños, estaré allí para detener el tiempo y crear recuerdos que os emocionarán y os harán revivir esa felicidad una y otra vez.',
+            'me.p3' : 'Mi compromiso es que os sintáis cómodos y en confianza en todo momento, para que podáis ser vosotros mismos y disfrutar al máximo de cada instante. Antes del gran día, hablaremos en profundidad sobre vuestros deseos y expectativas, para que podáis confiar en mí y sentir la tranquilidad de saber que vuestra historia está en buenas manos.',
+            'me.p4' : 'Cuando veáis las fotografías, podréis revivir esos momentos llenos de emoción y alegría, y sentir como el tiempo se detiene por un instante.',
+
+            'f1' : 'PLACER CULPABLE',
+            'ff1' : 'Siestas',
+            'f2' : 'Viaje soñado',
+            'ff2' : 'Países nórdicos',
+            'f3' : 'Apasionada por',
+            'ff3' : 'Los animales',
+            'f4' : 'No puedo vivir sin',
+            'ff4' : 'Mi gatita',
+            'f5' : 'Película favorita',
+            'ff5' : 'El señor de los anillos',
+            'f6' : 'Grupo de música favorito',
+            'ff6' : 'Guns N’ Roses',
+            'f7' : 'Playa o montaña',
+            'ff7' : 'Montaña siempre',
+            'f8' : 'Hobby curioso',
+            'ff8' : 'Adicta a los sudokus',
+
+            'timelineTitle' : 'MI VIAJE HASTA AHORA',
+            'timelineSubtitle' : 'DÓNDE EMPEZÓ TODO Y CÓMO VA AHORA',
+
+            'timeline.year1': "2012",
+            'timeline.title1': "Compro mi primera cámara.",
+            'timeline.description1': "La necesitaba para la asignatura de fotografía en la universidad. Así empecé a practicar y enamorarme.",
+            'timeline.imgSrc1': "/img/18.webp",
+            'timeline.alignment1': "left",
+
+            'timeline.year2': "2017",
+            'timeline.title2': "Le digo Sí a la fotografía y emprendo mi proyecto.",
+            'timeline.description2': "Finalizo mis estudios en Bellas Artes y decido empezar mi aventura como fotógrafa.",
+            'timeline.imgSrc2': "/img/19-1.webp",
+            'timeline.alignment2': "right",
+
+            'timeline.year3': "2018",
+            'timeline.title3': "De Barcelona a Lleida.",
+            'timeline.description3': "Cambio de ciudad y me instalo como fotógrafa en Lleida, especializada en bodas y parejas, sesiones individuales y con familias.",
+            'timeline.imgSrc3': "/img/20.webp",
+            'timeline.alignment3': "left",
+
+            'timeline.year4': "2023",
+            'timeline.title4': "De vuelta a mi querida Barcelona.",
+            'timeline.description4': "Una pandemia después vuelvo a la ciudad donde todo empezó, con muchas ganas e ilusión.",
+            'timeline.imgSrc4': "/img/21.webp",
+            'timeline.alignment4': "right",
+
+            'timelineFuture' : '2024 & Más allá',
         },
+
+        form: {
+            'title': "FORMULARIO",
+
+            'name': "Nombre",
+            'email': "Email",
+            'phone': "Teléfono",
+            'date': "Fecha del evento",
+            'location': "¿Dónde será?",
+
+            'reservationPlaceholder': "¿Qué quieres reservar?",
+            'reservationWedding': "Boda",
+            'reservationCouple': "Sesión de pareja",
+            'reservationIndividual': "Sesión individual",
+            'reservationFamily': "Sesión en familia",
+            'reservationOthers': "Otros",
+
+            'knownPlaceholder': "¿Dónde me has conocido?",
+            'knownInstagram': "Instagram",
+            'knownGoogle': "Google",
+            'knownRecommendation': "Recomendación",
+
+            'details': "Cuéntame más detalles",
+
+            'privacyPolicy': "Por favor, revisa la política de privacidad.",
+
+            'submit': "Enviar",
+        }
+
+    
+    }
 
         
     }
-} 
